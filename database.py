bdb14a6389d289ae721e25f18e016e04ef252c7e
SehrGuterCode <101972908+SehrGuterCode@users.noreply.github.com>
2023-02-19 17:07:35 +0000

    store application in db

--- database.py
+++ database.py
@@ -45,3 +45,20 @@ def load_job_as_json(id):
     return None
   else:
     return jsonify(tuple(rows[0]))
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    conn.execute(query,
+                 {
+                   'job_id': job_id,
+                   'full_name': data['full_name'],
+                   'email': data['email'],
+                   'linkedin_url': data['linkedin_url'],
+                   'education': data['education'],
+                   'work_experience': data['work_experience'],
+                   'resume_url': data['resume_url']
+                 }
+    )
