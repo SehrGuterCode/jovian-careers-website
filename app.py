1ed65845a3494116b9446b381615860ae96b8141
SehrGuterCode <101972908+SehrGuterCode@users.noreply.github.com>
2023-02-19 11:59:36 +0000

    some changes

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ JOBS = [
     'id': 4,
     'title': 'Backend Engineer',
     'location': 'San Francisco, USA',
-    'salary': '$120,000'
+    'salary': '$150,000'
   }
 ]
 
@@ -34,9 +34,11 @@ JOBS = [
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='Jovian')
 
+
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
