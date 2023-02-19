bf3b501280fee1075de45b6928da1a8712d83e20
SehrGuterCode <101972908+SehrGuterCode@users.noreply.github.com>
2023-02-19 16:07:34 +0000

    add dynamic pages for each job

--- database.py
+++ database.py
@@ -24,3 +24,24 @@ def load_jobs_as_json():
   results = load_jobs_result()
   results = [tuple(row) for row in results]
   return jsonify(results)
+
+
+def load_job_result_from_db(id):
+  with engine.connect() as conn:
+    return conn.execute(text(f"SELECT * FROM jobs WHERE ID = {id}"))
+
+
+def load_job_from_db(id):
+  rows = load_job_result_from_db(id).all()
+  if len(rows) == 0:
+    return None
+  else:
+    return rows[0]._mapping
+
+
+def load_job_as_json(id):
+  rows = load_job_result_from_db(id).all()
+  if len(rows) == 0:
+    return None
+  else:
+    return jsonify(tuple(rows[0]))
